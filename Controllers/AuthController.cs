3372f2f6ebb482b8b0fc9f3475fe0d2acaed3e23
Suvrat Mishra <suvrat29@gmail.com>
2020-04-15 15:18:43 +0530

    Changes for authentication and validations.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -22,8 +22,6 @@ namespace udemyApp.API.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            // validate request
-
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
 
             if (await _repo.UserExists(userForRegisterDto.Username))
