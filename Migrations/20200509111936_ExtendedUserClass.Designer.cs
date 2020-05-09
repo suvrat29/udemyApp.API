5bccbca12d788f357107bcfcc07dd2486cbab7b5
Suvrat Mishra <suvrat29@gmail.com>
2020-05-09 18:03:52 +0530

    Begin expanding the functionality of the API, seeding new data, and adding a migration seeder to the start of program. Added a logger for any errors due to this on the Program file.

--- Migrations/20200509111936_ExtendedUserClass.Designer.cs
+++ Migrations/20200509111936_ExtendedUserClass.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using udemyApp.API.Data;
 
 namespace udemyApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20200509111936_ExtendedUserClass")]
+    partial class ExtendedUserClass
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
+using udemyApp.API.Data;
+using udemyApp.API.Helpers;
 
 namespace udemyApp.API
 {
@@ -13,7 +13,26 @@ namespace udemyApp.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                    Seed.SeedUsers(context);
+                }
+                catch (Exception error)
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    Extensions.AddToApplicationLog(error.Message, error.Source, error.StackTrace, context);
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(error, "An error occured during migration");
+                }
+
+                host.Run();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
